3617a3d509e67c0a13ae9449f09f111d1e3a1f30
Abderraouf-Naji <naji.abderraouf@enis.tn>
2024-06-27 15:10:22 +0100

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": process.env.REACT_APP_BACKEND_URL,
+      "/api/": process.env.REACT_APP_BACKEND_URL ,
       "/uploads/": process.env.REACT_APP_BACKEND_URL,
     },
   },
 });
+
+
+// "/api/": "http://127:5000",
+//       "/uploads/": "http://localhost:5000",
